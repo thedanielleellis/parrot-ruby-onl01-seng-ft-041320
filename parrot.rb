f70b1512deb69351704cfb4555d1e5045d376910
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-14 07:02:50 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot ("Squawk!")
-   puts phrase
-   phrase = "Squawk!"
-   puts "Pretty Bird!"
-   return phrase
+def parrot (name)
+   puts "Squawk!"
+   Squawk
 end
+
+parrot("Squawk")
